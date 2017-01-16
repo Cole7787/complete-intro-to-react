b49eb885f327cc013ab6e774fdeab1511fe40062
Cole Allan <cacoleallan@gmail.com>
2017-01-15 20:48:23 -0700

    Rendering problem

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,28 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router'
-import App from './App'
+  var div = React.DOM.div
+  var h1 = React.DOM.h1
+  var MyTitle = React.createClass({
+    render: function(){
+      return (
+        div(null,
+          h1({style: {color: this.props.color}}, this.props.title)
+        )
+      )
+    }
+  })
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
+  var MyTitleFactory = React.createFactory(MyTitle)
+
+  var MyFirstComponent = React.createClass({
+    render: function(){
+      return (
+        div(null,
+        MyTitleFactory({title: 'props are the best', color: 'peru'}),
+        MyTitleFactory({title: 'props are the best', color: 'black'}),
+        MyTitleFactory({title: 'props are the best', color: 'tomato'}),
+        MyTitleFactory({title: 'props are the best', color: 'rebeccapurple'})
+      )
+    )
+  }
+})
+
+  ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
